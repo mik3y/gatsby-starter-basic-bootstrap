915346c84270bef07af42db2e5da34be54152b16
mike wakerly <opensource@hoho.com>
2020-04-05 10:05:10 -0400

    Create `gatsby-starter-basic-bootstrap`

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Basic Bootstrap Starter`,
+    description: `A simple Gatsby starter leveraging react-bootstrap and little else.`,
+    author: `@mik3y`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `gatsby-starter-basic-bootstrap`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
